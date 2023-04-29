faf5527768b754c446aafa165df46fd3f8065f43
AndriySachyk <asachyk1312@gmail.com>
2023-04-30 01:04:07 +0300

    Mistace

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+
+
+const listGalleryUl = document.querySelector(".gallery");
+
+const itemLi = ({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href=${original} data-original-img=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+    //   data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+
+listGalleryUl.innerHTML = galleryItems.map(itemLi).join('')
+
+
+const arryInstances = [];
+
+
+listGalleryUl.addEventListener('click', (even) => {
+    even.preventDefault()
+    const originalLink = even.target
+        .closest(".gallery__item")
+        .getAttribute("data-original-img");
+    
+    const instance = basicLightbox.create(`<img scr=${original} >`);
+    arryInstances.push(originalLink);
+    originalLink.show()
+    document.addEventListener("keydown", (even) => onEsc(even, instance))
+});
+
+
+const onEsc = (even, instance)=> {
+    const escCode = "Escape";
+    if (even.code === escCode) {
+        instance.close();
+        arryInstances.splice(arryInstances.indexOf(instance), 1)
+    
+        document.removeEventListener("keydown", (even) => onEsc(even, instance))
+    }
+}
